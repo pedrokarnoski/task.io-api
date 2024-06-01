2b9fbb1f78f8431092eefbd2e7d196877e3f2c4b
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-05-31 21:21:15 -0300

    fix: Rota user/id

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,13 +2,15 @@ const { checkAuthMiddleware } = require('../middlewares/checkAuthMiddleware');
 const controller = require('../controllers/UsersController');
 const router = require('express').Router();
 
+router.post('/', controller.createUser); /** /users **/
+
 // Middleware de autenticação
 router.use(checkAuthMiddleware);
 
 // CRUD Routes
+router.get('/me', controller.getMe); /** /users/me **/
 router.get('/', controller.getUsers); /** /users **/
 router.get('/:userId', controller.getUser); /** /users/:userId **/
-router.post('/', controller.createUser); /** /users **/
 router.put('/:userId', controller.updateUser); /** /users/:userId **/
 router.delete('/:userId', controller.deleteUser); /** /users/:userId **/
 
