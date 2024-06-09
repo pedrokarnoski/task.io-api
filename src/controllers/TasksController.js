1d37d7b162f203bc00e093bc7884b3b3bd704c28
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-06-08 21:51:55 -0300

    feat: Rota tasks

---
+++ src/controllers/TasksController.js
@@ -0,0 +1,122 @@
+const Task = require('../models/task');
+const AppError = require("../utils/AppError");
+
+// Get all tasks
+exports.getTasks = async (req, res, next) => {
+  try {
+    const tasks = await Task.findAll();
+
+    if (!tasks || tasks.length === 0) {
+      next(new AppError('Nenhum tarefa encontrado.', 404));
+    }
+
+    res.status(200).json({ tasks });
+  } catch (error) {
+    next(new AppError('Erro ao buscar tarefas.', 500));
+  }
+};
+
+// Get task by id
+exports.getTask = async (req, res, next) => {
+  try {
+    const { taskId } = req.params;
+
+    const task = await Task.findByPk(taskId);
+
+    if (!task) {
+      next(new AppError('Tarefa não encontrada.', 404));
+    }
+
+    res.status(200).json(task);
+  } catch (error) {
+    next(new AppError('Erro ao buscar tarefa.', 500));
+  }
+};
+
+// Create task
+exports.createTask = async (req, res, next) => {
+  try {
+    if (!req.user || !req.user.id) {
+      next(new AppError('Usuário não autenticado.', 401));
+    }
+
+    const userId = req.user.id;
+
+    const { description } = req.body;
+
+    if (!description) {
+      next(new AppError("Informe a descrição da tarefa."));
+    }
+
+    const task = await Task.create({ description, userId });
+
+    res.status(201).json({ task });
+  } catch (error) {
+    next(new AppError('Erro ao criar tarefa.', 500));
+  }
+};
+
+// Update task
+exports.updateTask = async (req, res, next) => {
+  try {
+    const { taskId } = req.params;
+    const { description, completed } = req.body;
+
+    const task = await Task.findByPk(taskId);
+
+    if (!task) {
+      next(new AppError("Tarefa não encontrada", 404));
+    }
+
+    if (!description && !completed) {
+      next(AppError("Informe a descrição da tarefa."));
+    }
+
+    task.description = description;
+    task.completed = completed;
+
+    await task.save();
+
+    return res.status(200).json(task);
+  } catch (error) {
+    console.error(error)
+    next(new AppError('Erro ao atualizar tarefa.', 500));
+  }
+};
+
+// Delete task
+exports.deleteTask = async (req, res, next) => {
+  try {
+    const { taskId } = req.params;
+
+    const task = await Task.findByPk(taskId);
+
+    if (!task) {
+      next(new AppError('Tarefa não encontrado.', 404));
+    }
+
+    await task.destroy();
+
+    res.status(204).send();
+  } catch (error) {
+    next(new AppError('Erro ao deletar tarefa.', 500));
+  }
+};
+
+// Current user tasks
+exports.getMy = async (req, res, next) => {
+  try {
+    const { userId } = req.user.id;
+
+    const tasks = await Task.findAll({ where: userId });
+
+    if (!tasks) {
+      next(new AppError('Sem tarefas cadastradas.', 404));
+    }
+
+    res.status(200).json({ tasks });
+  } catch (error) {
+    console.log(error);
+    next(new AppError('Erro ao buscar usuário.', 500));
+  }
+};
