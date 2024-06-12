616763631aaefa21c378174f10a321a9c2c060b8
Marcus Toledo <marcus_viniciustoledo@hotmail.com>
2024-06-12 09:22:17 -0300

    refactor: Ajustado nome da tabela

--- src/models/comments.js
+++ src/models/comments.js
@@ -1,22 +1,24 @@
-const { DataTypes } = require('sequelize');
-const db = require('../config/database');
+const { DataTypes } = require("sequelize");
+const db = require("../config/database");
 
-const Comment = db.define('Comments', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    comment: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    taskId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    });
+const Comments = db.define("comments", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  comment: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  taskId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+module.exports = Comments;
