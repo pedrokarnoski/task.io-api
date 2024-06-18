4f4203dc917fd52c3f14752a193da08e151d347a
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-06-18 15:38:21 -0300

    fix: Dialect banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.POSTGRES_CONNECTION);
+const sequelize = new Sequelize(
+  process.env.POSTGRES_CONNECTION, {
+  dialect: 'postgres',
+});
 
 module.exports = sequelize;
