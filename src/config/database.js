93b361b73126fa66c61f52d3b9a9308bd8679f53
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-06-18 15:24:43 -0300

    feat: Configurar deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,5 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.POSTGRES_DB,
-  process.env.POSTGRES_USER,
-  process.env.POSTGRES_PASSWORD,
-  {
-    host: process.env.POSTGRES_HOST,
-    dialect: 'postgres',
-  }
-);
+const sequelize = new Sequelize(process.env.POSTGRES_CONNECTION);
 
 module.exports = sequelize;
