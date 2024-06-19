da06e6cb10e29c4e2c3f9a12ab77cae9185188fb
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-06-19 15:59:45 -0300

    fix: CORS

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -1,7 +1,21 @@
 const cors = require('cors');
 
+const allowedOrigins = {
+  development: process.env.DEV_ORIGIN,
+  production: process.env.PROD_ORIGIN
+};
+
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: function (origin, callback) {
+    const env = process.env.NODE_ENV || 'development';
+    const allowedOrigin = allowedOrigins[env];
+
+    if (origin === allowedOrigin || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
